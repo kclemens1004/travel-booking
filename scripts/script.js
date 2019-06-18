299bb6a77acb4e3b51fbf51b3ef00f6e8760af80
kclemens1004 <kennethwclemens@gmail.com>
2019-06-17 21:17:36 -0400

    added hotel objects and worked on selctor logic

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,49 @@
 var hotelButton = document.getElementById('hotel-select');
-var hotel = document.getElementById('hotel');
+var hotelInfo = document.getElementById('hotel-info');
+
+
+var redBoofInn = {
+    name: "Red Boof Inn",
+    id: 1,
+    image: "<img src='img/redboof.jpg' alt='redboof'>",
+    description: "red boof description"
+};
+
+var ebdb = {
+    name: "EBDB BnB",
+    id: 2,
+    image: "<img src='img/ebdb.jpg' alt='ebdb'>",
+    description: "ebdb description"
+};
+
+var stufittInn = {
+    name: "Stufitt Inn",
+    id: 3,
+    image: "<img src='img/stufitt.jpg' alt='redboof'>",
+    description: "stufitt description"
+};
 
 function selectHotel() {
-    console.log(hotel.value);
+    var hotel = document.getElementById('hotel');
+    hotelInfo.style.display = "flex";
+
+    if (hotel.value === "redBoofInn") {
+        console.log(redBoofInn.name);
+
+    }
+
+    else if (hotel.value === "ebdb") {
+        console.log(ebdb.name);
+    }
+
+    else if (hotel.value === "stufittInn") {
+        console.log(stufittInn.name)
+    }
+
+    else {
+        console.log("error");
+    }
+
 }
 
 hotelButton.onclick = selectHotel;
